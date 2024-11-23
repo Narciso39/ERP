5dd1cbb30ae4622d0a656b0d12b1f44677fc18bd
Luis Felipe <felipenarciso15@gmail.com>
2024-11-23 12:50:33 -0300

    feat: criei o método de criação de usúario

--- src/users/user.controller.ts
+++ src/users/user.controller.ts
@@ -1,6 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { UserService } from './user.service';
+import { UserCreateDTO } from './dto/user-create.dto';
 
 @Controller()
 export class UserController {
-//   @Get()
+    constructor(private readonly UserService: UserService){}
+    @Post()
+   async create(@Body() user: UserCreateDTO) {
+        // this.UserService.store(user);
+    }
 }
--- src/users/user.service.ts
+++ src/users/user.service.ts
@@ -1,6 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { UserCreateDTO } from './dto/user-create.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class UserService {
- 
+    constructor(private readonly prisma: PrismaService) {}
+
+    async store(newUser: UserCreateDTO) {
+        return this.prisma.user.create({
+            data: newUser
+        });
+    }
+  
 }
